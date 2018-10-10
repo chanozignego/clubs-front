216adbee7f421c985ef0ff2a12b0aa392c3243a9
agustinwalas <agustinwalas@gmail.com>
2018-10-10 17:13:30 -0300

    login/register & emergencies

--- www/js/controllers/application-controller.js
+++ www/js/controllers/application-controller.js
@@ -39,4 +39,37 @@ angular
         $scope.closeLogin();
       }, 1000);
     };
-  })
+
+
+
+  // Form data for the register modal
+  $scope.RegisterData = {};
+
+  // Create the register modal that we will use later
+  $ionicModal.fromTemplateUrl('templates/register.html', {
+    scope: $scope
+  }).then(function(modal) {
+    $scope.modal = modal;
+  });
+
+  // Triggered in the register modal to close it
+  $scope.closeRegister = function() {
+    $scope.modal.hide();
+  };
+
+  // Open the Register modal
+  $scope.register = function() {
+    $scope.modal.show();
+  };
+
+  // Perform the login action when the user submits the login form
+  $scope.doRegister = function() {
+    console.log('Doing register', $scope.registerData);
+
+    // Simulate a register delay. Remove this and replace with your register
+    // code if using a register system
+    $timeout(function() {
+      $scope.closeRegister();
+    }, 1000);
+  };
+})
--- www/js/controllers/emergencies-controller.js
+++ www/js/controllers/emergencies-controller.js
@@ -2,11 +2,11 @@ angular
   .module('clubs-front')
   .controller('EmergenciesController', function($scope) {
     $scope.emergencies = [
-      { title: 'Vuelve Music & House', id: 1 },
-      { title: 'Nuevo! Ampiación de proveduría', id: 2 },
-      { title: 'Recomendaciones para limpieza de tanques', id: 3 },
-      { title: 'La eficiencia energética', id: 4 },
-      { title: 'Golf: Torneo inter clubes', id: 5 },
+      { title: 'Llamar Emergencias del club', description: 'Descripcion', id: 1, },
+      { title: 'Llamar Policia',description: 'Descripcion', id: 2 },
+      { title: 'Llamar Bomberos',description: 'Descripcion', id: 3 },
+      { title: 'Llamar Asistencia medica',description: 'Descripcion', id: 4 },
+
 
     ];
   })
