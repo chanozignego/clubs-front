7f5f915f8335d61db1ec051c474cacc949d875a9
Esteban Zignego <esteban.zignego@gmail.com>
2018-10-12 10:40:11 -0300

    add useful data tabs

--- www/js/controllers/useful-data-controller.js
+++ www/js/controllers/useful-data-controller.js
@@ -1,12 +1,22 @@
 angular
   .module('clubs-front')
   .controller('UsefulDataController', function($scope) {
-    $scope.posts = [
-      { title: 'Vuelve Music & House', id: 1 },
-      { title: 'Nuevo! Ampiación de proveduría', id: 2 },
-      { title: 'Recomendaciones para limpieza de tanques', id: 3 },
-      { title: 'La eficiencia energética', id: 4 },
-      { title: 'Golf: Torneo inter clubes', id: 5 },
-      { title: 'Tu vida sin bolsas de plástico', id: 6 }
+
+    $scope.contacts = [
+      {id: "1", name: "Emergencias", description: "Interno 1911", phone: "(011) 3221-7101", email: ""},
+      {id: "2", name: "Acceso Guardia Securitas", description: "Interno 1104", phone: "(011) 3221-7101", email: ""},
+      {id: "3", name: "Conmutador", description: "", phone: "", email: "conmutador@simpit.com"},
+      {id: "4", name: "Administración", description: "", phone: "0800-3221", email: ""},
+      {id: "5", name: "Secretaría Golf", description: "Interno 1110", phone: "(011) 3221-7101", email: ""},
+      {id: "6", name: "Secretaría Sector Deportivo", description: "", phone: "", email: "sectordeportivo@simpit.com"}
     ];
+
+    $scope.documents = [
+
+    ];
+
+    $scope.instructions = [
+
+    ];
+
   })
