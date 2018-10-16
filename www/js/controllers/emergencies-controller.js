da92137125c442312a9d34de62249e9e3fad715a
agustinwalas <agustinwalas@gmail.com>
2018-10-16 15:01:13 -0300

    fix login-register

--- www/js/controllers/emergencies-controller.js
+++ www/js/controllers/emergencies-controller.js
@@ -2,10 +2,13 @@ angular
   .module('clubs-front')
   .controller('EmergenciesController', function($scope) {
     $scope.emergencies = [
-      { title: 'Llamar Emergencias del club', description: 'Descripcion', id: 1, },
-      { title: 'Llamar Policia',description: 'Descripcion', id: 2 },
-      { title: 'Llamar Bomberos',description: 'Descripcion', id: 3 },
-      { title: 'Llamar Asistencia medica',description: 'Descripcion', id: 4 },
+      { title: 'Llamar Policia',description: '911', id: 1 },
+      { title: 'Llamar Bomberos',description: '110', id: 2 },
+      { title: 'Llamar Emergencias del club', description: 'Interno 1380', id: 3, },
+      { title: 'Acceso Guardia Securitas',description: 'Interno 1832', id: 4 },
+      { title: 'Administracion',description: 'Interno 114', id: 5 },
+      { title: 'Llamar Asistencia medica',description: 'Asistencia interna del Club', id: 6 },
+
 
 
     ];
--- www/js/controllers/reservation-controller.js
+++ www/js/controllers/reservation-controller.js
@@ -1,11 +1,9 @@
 angular
   .module('clubs-front')
-  .controller('ReservationsController', function($scope) {
+  .controller('ReservationController', function($scope, $stateParams) {
     $scope.reservations = [
       { id: 1, bookeable:'Cancha de tenis',  date: '15/08/2018', time:'13:30 - 15:00', time_used: '1:30 horas', comments:'comentario',description:'Cancha 4', price:'250' },
       { id: 2, bookeable:'Cancha de futbol',  date: '10/08/2018', time:'13:30 - 15:00', time_used: '30 minutos', comments:'comentario',description:'Cancha 2', price:'300' },
       { id: 3, bookeable:'Quincho',  date: '5/08/2018', time:'13:30 - 15:00', time_used: '2 horas', comments:'comentario', description:'Quincho subsuelo', price:'450' },
       { id: 4, bookeable:'Paddle',  date: '12/07/2018', time:'13:30 - 15:00', time_used: '1 hora', comments:'comentario', description:'Cancha 6', price:'200' },
-
-    ];
   })
