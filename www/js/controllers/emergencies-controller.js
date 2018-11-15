955f29a2d0c825849d3e48994e5f11146b051ceb
agustinwalas <agustinwalas@gmail.com>
2018-11-15 13:25:44 -0300

    design fixes

--- www/js/controllers/emergencies-controller.js
+++ www/js/controllers/emergencies-controller.js
@@ -2,11 +2,11 @@ angular
   .module('clubs-front')
   .controller('EmergenciesController', function($scope) {
     $scope.emergencies = [
-      { title: 'Llamar Policia', description: '911', id: 1 },
-      { title: 'Llamar Bomberos', description: '110', id: 2 },
-      { title: 'Llamar Emergencias del club', description: 'Interno 1380', id: 3, },
+      { title: 'Teléfono Policía', description: '911', id: 1 },
+      { title: 'Teléfono Bomberos', description: '110', id: 2 },
+      { title: 'Teléfono Emergencias del club', description: 'Interno 1380', id: 3, },
       { title: 'Acceso Guardia Securitas', description: 'Interno 1832', id: 4 },
-      { title: 'Administracion', description: 'Interno 114', id: 5 },
-      { title: 'Llamar Asistencia medica', description: 'Asistencia interna del Club', id: 6 }
+      { title: 'Administración', description: 'Interno 114', id: 5 },
+      { title: 'Teléfono Asistencia médica', description: 'Asistencia interna del Club', id: 6 }
     ];
   });
--- www/js/controllers/emergency-controller.js
+++ www/js/controllers/emergency-controller.js
@@ -1,5 +1,5 @@
 angular
   .module('clubs-front')
   .controller('EmergencyController', function($scope, $stateParams) {
-    $scope.emergency = { title: 'Llamar Policia', address: 'Av. Santa Fe 2255', description: '911', id: 1 };
+    $scope.emergency = { title: 'Telefono Policia', address: 'Av. Santa Fe 2255', description: '911', id: 1 };
   })
